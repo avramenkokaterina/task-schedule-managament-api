dda2076d9b425808fed536b2cbed3cf5b7962d93
Kateryna <k.avramenko111@gmail.com>
2020-06-08 23:08:46 +0300

    Config for heroku.

--- config/database.js
+++ config/database.js
@@ -1,9 +1,12 @@
 'use strict';
 
 module.exports = {
-  host: '127.0.0.1',
+  host: 'ec2-176-34-97-213.eu-west-1.compute.amazonaws.com',
   port: 5432,
-  database: 'postgres',
-  user: 'postgres',
-  password: 'admin',
+  database: 'd2hmlurtvqg3d6',
+  user: 'qqnenpoqrbpfoq',
+  password: '9e4f0ee0378749ae659d063ff8c7131c45c720103556ab712d638cade83ab281',
+  ssl: {
+    rejectUnauthorized: false
+  }
 };
